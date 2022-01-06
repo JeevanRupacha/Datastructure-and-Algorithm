106420985acc38cae2e4a206e59f66e428e4452b
Jeevan Rupacha <rupachasgvn@gmail.com>
2022-01-06 17:32:45 +0545

    Junit is removed

--- Queue/LQueueTest.java
+++ Queue/LQueueTest.java
@@ -1,18 +1,11 @@
 package Queue;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-
 public class LQueueTest {
-    
+
     LQueue lQueue;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         LQueue lQueue = new LQueue();
-
 
         lQueue.enqueue("item1");
         lQueue.enqueue("item2");
@@ -27,31 +20,21 @@ public class LQueueTest {
 
     }
 
-    @Before
-    public void estUp() throws Exception
-    {
-        lQueue = new LQueue();
-    }
+    // @Before
+    // public void estUp() throws Exception {
+    // lQueue = new LQueue();
+    // }
 
-    @Test
-    public void enqueues()
-    {
-        lQueue.enqueue("item1");
-        Assert.assertEquals("item1", lQueue.dequeue());
-        lQueue.enqueue("item2");
-        Assert.assertEquals("item2", lQueue.dequeue());
-        lQueue.enqueue("item3");
-        lQueue.enqueue("item4");
-        lQueue.enqueue("item5");
-        Assert.assertEquals("item5", lQueue.dequeue());
-    }
+    // @Test
+    // public void enqueues() {
+    // lQueue.enqueue("item1");
+    // Assert.assertEquals("item1", lQueue.dequeue());
+    // lQueue.enqueue("item2");
+    // Assert.assertEquals("item2", lQueue.dequeue());
+    // lQueue.enqueue("item3");
+    // lQueue.enqueue("item4");
+    // lQueue.enqueue("item5");
+    // Assert.assertEquals("item5", lQueue.dequeue());
+    // }
 
-    @Test
-    public void dequeues()
-    {
-        lQueue.dequeue();
-        lQueue.dequeue();
-        lQueue.dequeue();
-        // Assert.assertEquals()
-    }
 }
--- Stack/StackTest.java
+++ Stack/StackTest.java
@@ -1,39 +1,35 @@
 package Stack;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 public class StackTest {
     MyStack stack;
 
-    @Before
-    public void setUp() throws Exception{
-        stack = new MyStack();
-     }
+    // @Before
+    // public void setUp() throws Exception{
+    // stack = new MyStack();
+    // }
 
-    @Test
-    public void pop(){
-        stack.push("test2");
-        stack.push("item2");
-        Assert.assertEquals("item2", stack.pop());
-        stack.push("item3");
-        stack.push("item4");
-        stack.push("item5");
-        Assert.assertEquals("item5", stack.pop());
-    }
+    // @Test
+    // public void pop(){
+    // stack.push("test2");
+    // stack.push("item2");
+    // Assert.assertEquals("item2", stack.pop());
+    // stack.push("item3");
+    // stack.push("item4");
+    // stack.push("item5");
+    // Assert.assertEquals("item5", stack.pop());
+    // }
 
-    @Test
-    public void push(){
-        stack.push("test2");
-        stack.push("item2");
-        Assert.assertEquals(1, stack.top);
-        stack.push("item3");
-        stack.push("item4");
-        Assert.assertEquals(3, stack.top);
+    // @Test
+    // public void push(){
+    // stack.push("test2");
+    // stack.push("item2");
+    // Assert.assertEquals(1, stack.top);
+    // stack.push("item3");
+    // stack.push("item4");
+    // Assert.assertEquals(3, stack.top);
 
-        stack.push("item5");
-        Assert.assertEquals(4, stack.top);
+    // stack.push("item5");
+    // Assert.assertEquals(4, stack.top);
 
-    }
+    // }
 }
